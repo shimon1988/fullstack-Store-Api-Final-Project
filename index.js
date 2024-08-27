73e2fddc78eaaf5a7434fae11d57b71f872dc632
s-mos <s-mos@LAPTOP-JG48IH6L>
2024-08-27 19:14:40 +0300

    added update product

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.use(express.json());
 
 
 const filePath = path.join(__dirname, 'tasks.json');
+const deleteFilePath = path.join(__dirname, 'deletedTasks.json');
 
 
+// load products in tasks.json
 function loadTasks() {
     if (fs.existsSync(filePath)) {
         const fileContent = fs.readFileSync(filePath, 'utf-8');
@@ -20,7 +22,7 @@ function loadTasks() {
     return [];
 }
 
-
+// save products in tasks.json
 function saveTasks(tasks) {
     fs.writeFileSync(filePath, JSON.stringify(tasks, null, 2), 'utf-8');
 }
@@ -45,7 +47,7 @@ app.post('/addTask', (req, res) => {
     res.status(201).json(newTask);
 });
 
-// get a task by id 
+// get a product by id 
 app.get('/getTask/:id', (req, res) => {
     const taskId = req.params.id;
     const task = taskArray.find(task => task.id === taskId);
@@ -54,6 +56,23 @@ app.get('/getTask/:id', (req, res) => {
     }
     res.json(task);
 })
+
+app.put('/updateTask/:id', (req, res) => {
+    const taskId = req.params.id;
+    const updatedTaskData = req.body;
+
+    const taskIndex = taskArray.findIndex(task => task.id === taskId);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({ error: 'Task not found' });
+    }
+
+    taskArray[taskIndex] = { ...taskArray[taskIndex], ...updatedTaskData };
+
+    saveTasks(taskArray);
+
+    res.status(200).json(taskArray[taskIndex]);
+});
 
 app.get("/", (req, res) => {
     res.json(taskArray);
